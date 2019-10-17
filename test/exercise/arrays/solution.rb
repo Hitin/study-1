c6351cacc70a42768b632c28ccf000ca2f1034ed
Petr Ablyazov <phoenix-h@mail.ru>
2019-10-17 22:25:05 +0400

    Array. Exercise search

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -9,7 +9,11 @@ module Exercise
       end
 
       def search(_array, _query)
-        0
+      #  res = _array.index(_query)
+      #  res.nil? ? -1 : res
+        res = -1; 
+        _array.each_with_index {|val, ind| res = ind if (val == _query)}
+        res
       end
     end
   end
