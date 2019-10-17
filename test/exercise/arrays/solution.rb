cce73d348b7c6be1267ad88eeb9966e81a865232
Petr Ablyazov <phoenix-h@mail.ru>
2019-10-17 21:46:43 +0400

    Array. Exercise replace

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,10 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        maxValue = array.max
+        res = []
+        array.each {|val| val > 0 ? res << maxValue : res << val}
+        res
       end
 
       def search(_array, _query)
