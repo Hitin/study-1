e4a58dfdaf46cf84c9ae200ccde86f0fc803585f
Petr Ablyazov <phoenix-h@mail.ru>
2019-10-18 11:36:15 +0400

    Fp.Exercise rating.

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,7 +5,8 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(_array)
-        0
+        result = _array.map { |film| film["rating_kinopoisk"].to_f if (!film["country"].nil? && film["country"].split(',').size > 1 && film["rating_kinopoisk"].to_f > 0) }.compact
+        result.reduce(:+) / result.size
       end
 
       def chars_count(_films, _threshold)
