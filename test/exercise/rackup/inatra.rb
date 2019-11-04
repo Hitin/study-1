be4f8c287e7e0ce9be77c869576719daa223f05c
Petr Ablyazov <phoenix-h@mail.ru>
2019-11-04 15:12:10 +0400

    My rackup

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,9 +1,16 @@
 module Inatra
   class << self
-    def routes
+    def routes(&block)
+      instance_eval(&block)
     end
 
-    def call(env)
+    def call(env)   	
+      @request.call(env)
     end
+
+    def get(val, &block)
+      @request = block
+    end
+
   end
 end
