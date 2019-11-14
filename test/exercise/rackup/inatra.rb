7b7c658a0dd280713e19f423a297b2fd6a537718
Petr Ablyazov <phoenix-h@mail.ru>
2019-11-14 15:35:21 +0400

    Add methods CRUD. Check path.

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -5,10 +5,29 @@ module Inatra
     end
 
     def call(env)   	
+      req = Rack::Request.new(env)
+      path_info = req.path_info
+      return [404,{},['No']] unless @path == path_info
       @request.call(env)
     end
 
-    def get(val, &block)
+    def get(path, &block)
+      @path = path
+      @request = block
+    end
+
+    def post(path, &block)
+      @path = path
+      @request = block
+    end
+
+    def delete(path, &block)
+      @path = path
+      @request = block
+    end
+
+    def put(path, &block)
+      @path = path
       @request = block
     end
 
